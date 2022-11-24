247ef64f2cc1b0b61f31b34db6293ef6eb3f0705
Teo Jansson Minne <teojm@kth.se>
2022-11-24 20:13:42 +0100

    Resolved typing issues

--- Titanic/titanic-feature-pipeline-daily.py
+++ Titanic/titanic-feature-pipeline-daily.py
@@ -1,31 +1,31 @@
 import os
 import modal
 
-LOCAL=True
+LOCAL = False
 
 if LOCAL == False:
    stub = modal.Stub()
-   image = modal.Image.debian_slim().pip_install(["hopsworks==3.0.4"])
+   image = modal.Image.debian_slim().apt_install(["libgomp1"]).pip_install(["hopsworks", "seaborn", "joblib","dataframe-image", "sklearn"])
 
    @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
    def f():
        g()
 
 
-def generate_passenger(pclass_max, pclass_min, sex, age_max, age_min, sibsp_max, sibsp_min, parch_max,
-                       parch_min, pricerange_max, pricerange_min, survived):
+def generate_passenger(pclass_min, pclass_max, sex, age_min, age_max, sibsp_min, sibsp_max, parch_min,
+                       parch_max, pricerange_min, pricerange_max, survived):
     """
     Returns a single passenger as a single row in a DataFrame
     """
     import pandas as pd
     import random
 
-    df = pd.DataFrame({"pclass": [random.randint(pclass_min, pclass_max)],
-                       "sex": [sex],
-                       "age": [random.randint(age_min, age_max)],
-                       "sibsp": [random.randint(sibsp_min, sibsp_max)],
-                       "parch": [random.randint(parch_min, parch_max)],
-                       "pricerange": [random.randint(pricerange_min, pricerange_max)]
+    df = pd.DataFrame({"pclass": [str(random.randint(pclass_min, pclass_max))],
+                       "sex": [str(sex)],
+                       "age": [random.uniform(age_min, age_max)],
+                       "sibsp": [str(random.randint(sibsp_min, sibsp_max))],
+                       "parch": [str(random.randint(parch_min, parch_max))],
+                       "pricerange": [str(random.randint(pricerange_min, pricerange_max))]
                       })
     df['survived'] = survived
     return df
@@ -35,25 +35,19 @@ def get_random_passenger():
     """
     Returns a DataFrame containing one random passenger
     """
-    import pandas as pd
     import random
 
-    #Surviving passenger
-    survivor_df = generate_passenger(1, 2, 1, 0, 80, 0, 8, 0, 6, 2, 5, 1)
-    #Non surviving passenger
-    non_survivor_df = generate_passenger(2, 3, 0, 0, 80, 0, 8, 0, 6, 1, 2, 0)
-
-
-    # randomly pick one of these and write it to the featurestore
+    # randomly pick one type of passenger
+    # and write it to the featurestore
     pick_random = random.randint(0, 1)
     if pick_random == 1:
-        titanic_df = survivor_df
+        df = generate_passenger(1, 2, 1, 0, 80, 0, 8, 0, 6, 2, 5, 1)
         print("Survivor added")
     else:
-        titanic_df = non_survivor_df
+        df = generate_passenger(2, 3, 0, 0, 80, 0, 8, 0, 6, 1, 2, 0)
         print("Non survivor added")
 
-    return titanic_df
+    return df
 
 
 def g():
@@ -62,10 +56,10 @@ def g():
     project = hopsworks.login()
     fs = project.get_feature_store()
 
-    titanic_df = get_random_passenger()
+    df = get_random_passenger()
 
-    titanic_fg = fs.get_feature_group(name="titanic_modal",version=1)
-    titanic_fg.insert(titanic_df, write_options={"wait_for_job" : False})
+    fg = fs.get_feature_group(name="titanic_modal_v2",version=1)
+    fg.insert(df, write_options={"wait_for_job" : False})
 
 if __name__ == "__main__":
     if LOCAL == True :
